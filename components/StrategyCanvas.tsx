52518c294320c61c674002e971259b10aee7def7
ORYNTECH <wikolis977@gmail.com>
2026-06-16 19:12:54 +0700

    Create StrategyCanvas.tsx

---
+++ components/StrategyCanvas.tsx
@@ -0,0 +1,9 @@
+export default function StrategyCanvas() {
+  return (
+    <div className="border rounded-2xl h-[600px] flex items-center justify-center">
+      <span className="text-gray-500">
+        Strategy Canvas
+      </span>
+    </div>
+  );
+}
